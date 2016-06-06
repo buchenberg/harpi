65881df74e105220a319f1062ef761d9f83681c4
G Buchenberger <buchenberger@gmail.com>
2016-06-05 23:28:46 -0600

    fdhdthj

--- modules/hars/server/controllers/hars.server.controller.js
+++ modules/hars/server/controllers/hars.server.controller.js
@@ -33,9 +33,22 @@ exports.create = function(req, res) {
 exports.read = function(req, res) {
   // convert mongoose document to JSON
   var har = req.har ? req.har.toJSON() : {};
+  var jolt = 'C:\\Users\\Greg\\APPS\\jolt-cli-0.0.22-SNAPSHOT-shaded.jar';
+  var spec = 'C:\\Users\\Greg\\Documents\\GitHub\\harpi\\modules\\hars\\server\\controllers\\spec.json';
+  var input = 'C:\\Users\\Greg\\Documents\\GitHub\\harpi\\modules\\projects\\client\\uploads\\projects\\57510af1f695f2801a399cf7\\har\\5092278d1379842f9462ba1e0a9fb847';
+  var exec = require('child_process').exec,
+    child;
+  child = exec('java -jar '+jolt+' transform '+spec+' '+input,
+    function(error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
 
-  // Add a custom field to the Article, for determining if the current User is the "owner".
-  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Article model.
+  // Add a custom field to Har, for determining if the current User is the "owner".
+  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Har model.
   har.isCurrentUserOwner = req.user && har.user && har.user._id.toString() === req.user._id.toString() ? true : false;
 
   res.jsonp(har);
@@ -45,9 +58,9 @@ exports.read = function(req, res) {
  * Update a Har
  */
 exports.update = function(req, res) {
-  var har = req.har ;
+  var har = req.har;
 
-  har = _.extend(har , req.body);
+  har = _.extend(har, req.body);
 
   har.save(function(err) {
     if (err) {
@@ -64,7 +77,7 @@ exports.update = function(req, res) {
  * Delete an Har
  */
 exports.delete = function(req, res) {
-  var har = req.har ;
+  var har = req.har;
 
   har.remove(function(err) {
     if (err) {
@@ -80,7 +93,7 @@ exports.delete = function(req, res) {
 /**
  * List of Hars
  */
-exports.list = function(req, res) { 
+exports.list = function(req, res) {
   Har.find().sort('-created').populate('user', 'displayName').exec(function(err, hars) {
     if (err) {
       return res.status(400).send({
@@ -103,7 +116,7 @@ exports.harByID = function(req, res, next, id) {
     });
   }
 
-  Har.findById(id).populate('user', 'displayName').exec(function (err, har) {
+  Har.findById(id).populate('user', 'displayName').exec(function(err, har) {
     if (err) {
       return next(err);
     } else if (!har) {
---
+++ query.jq
@@ -0,0 +1,2 @@
+let $har := collection("./modules/projects/uploads/projects/57510af1f695f2801a399cf7/har/5092278d1379842f9462ba1e0a9fb847")
+return $har
