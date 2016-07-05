cfeda4d419ddf15f6ed6c95500ed192ba4148b57
Greg Buchenberger <greggbuchenberger@admin.local>
2016-07-05 12:22:18 -0600

    nav

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Projects',
+  function ($scope, Authentication, Projects) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.projects = Projects.query();
   }
 ]);
--- modules/specs/client/config/specs.client.config.js
+++ modules/specs/client/config/specs.client.config.js
@@ -1,32 +1,6 @@
 (function () {
   'use strict';
 
-  angular
-    .module('specs')
-    .run(menuConfig);
+  angular.module('specs');
 
-  menuConfig.$inject = ['Menus'];
-
-  function menuConfig(Menus) {
-    // Set top bar menu items
-    Menus.addMenuItem('topbar', {
-      title: 'Specs',
-      state: 'specs',
-      type: 'dropdown',
-      roles: ['*']
-    });
-
-    // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'specs', {
-      title: 'List Specs',
-      state: 'specs.list'
-    });
-
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'specs', {
-      title: 'Create Spec',
-      state: 'specs.create',
-      roles: ['user']
-    });
-  }
 })();
