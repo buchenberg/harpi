1f17de698b9b6bb9b96ed8796c691dedc13e3533
Gregory Buchenberger <buchenberger@gmail.com>
2016-07-06 23:37:01 -0600

    fun with mongoose

--- modules/projects/server/controllers/projects.server.controller.js
+++ modules/projects/server/controllers/projects.server.controller.js
@@ -14,11 +14,11 @@ var path = require('path'),
 /**
  * Create a project
  */
-exports.create = function(req, res) {
+exports.create = function (req, res) {
   var project = new Project(req.body);
   project.user = req.user;
 
-  project.save(function(err) {
+  project.save(function (err) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
@@ -32,20 +32,20 @@ exports.create = function(req, res) {
 /**
  * Show the current project
  */
-exports.read = function(req, res) {
+exports.read = function (req, res) {
   res.json(req.project);
 };
 
 /**
  * Update a project
  */
-exports.update = function(req, res) {
+exports.update = function (req, res) {
   var project = req.project;
 
   project.title = req.body.title;
   project.description = req.body.description;
 
-  project.save(function(err) {
+  project.save(function (err) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
@@ -59,10 +59,10 @@ exports.update = function(req, res) {
 /**
  * Delete an project
  */
-exports.delete = function(req, res) {
+exports.delete = function (req, res) {
   var project = req.project;
 
-  project.remove(function(err) {
+  project.remove(function (err) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
@@ -76,16 +76,21 @@ exports.delete = function(req, res) {
 /**
  * List of Projects
  */
-exports.list = function(req, res) {
-  Project.find().sort('-created').populate('user', 'displayName').exec(function(err, projects) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.json(projects);
-    }
-  });
+//  'user', 'displayName'
+exports.list = function (req, res) {
+  Project.find().sort('-created')
+    .populate({
+      path: 'hars.name',
+      model: 'Har'
+    }).exec(function (err, projects) {
+      if (err) {
+        return res.status(400).send({
+          message: errorHandler.getErrorMessage(err)
+        });
+      } else {
+        res.json(projects);
+      }
+    });
 };
 
 
@@ -93,7 +98,7 @@ exports.list = function(req, res) {
 /**
  * Update har
  */
-exports.uploadHar = function(req, res) {
+exports.uploadHar = function (req, res) {
   var user = req.user,
     project = req.project,
     projectId = project._id,
@@ -105,7 +110,7 @@ exports.uploadHar = function(req, res) {
     console.log(user.displayName + ' is uploading a har file to the ' + req.project.title + ' project.');
     console.log("Uploading har to memory");
     //console.log(user.displayName + ' is adding a har file to the ' + req.project.title + ' project.');
-    upload(req, res, function(err) {
+    upload(req, res, function (err) {
       if (err) {
         console.log(err);
         return res.status(400).send({
@@ -113,10 +118,10 @@ exports.uploadHar = function(req, res) {
         });
       } else {
         var harJson = JSON.parse(req.file.buffer),
-        newHar = new Har(harJson);
+          newHar = new Har(harJson);
         newHar.name = req.file.originalname;
         newHar.user = user;
-        newHar.save(function(err) {
+        newHar.save(function (err) {
           if (err) {
             console.error(errorHandler.getErrorMessage(err));
             return res.status(400).send({
@@ -127,7 +132,7 @@ exports.uploadHar = function(req, res) {
           }
         });
         project.hars.push(newHar._id);
-        project.save(function(err) {
+        project.save(function (err) {
           if (err) {
             console.log(errorHandler.getErrorMessage(err));
             return res.status(400).send({
@@ -137,7 +142,7 @@ exports.uploadHar = function(req, res) {
             console.log('Har added to project.');
           }
         });
-         return res.json(project);
+        return res.json(project);
       }
     });
   } else {
@@ -150,7 +155,7 @@ exports.uploadHar = function(req, res) {
 /**
  * Project middleware
  */
-exports.projectByID = function(req, res, next, id) {
+exports.projectByID = function (req, res, next, id) {
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({
@@ -158,7 +163,10 @@ exports.projectByID = function(req, res, next, id) {
     });
   }
 
-  Project.findById(id).populate('user', 'displayName').exec(function(err, project) {
+  Project.findById(id)
+  .populate('user', 'displayName')
+  .populate('hars', 'name')
+  .exec(function (err, project) {
     if (err) {
       return next(err);
     } else if (!project) {
