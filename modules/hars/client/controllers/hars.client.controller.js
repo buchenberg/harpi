22337b95b2309a9a4a490b1cc6708c401bd8fd1c
Gregory Buchenberger <buchenberg@gmail.com>
2016-09-22 00:22:10 -0600

    stuff

--- modules/hars/client/controllers/hars.client.controller.js
+++ modules/hars/client/controllers/hars.client.controller.js
@@ -1,100 +1,127 @@
-(function () {
-  'use strict';
+(function() {
 
-  // Hars controller
-  angular
-    .module('hars')
-    .controller('HarsController', HarsController);
+    'use strict';
 
-  HarsController.$inject = ['$scope', '$state', 'Authentication',
-    'harResolve', '$timeout'
-  ];
+    // Hars controller
+    angular
+        .module('hars')
+        .controller('HarsController', HarsController);
 
-  function HarsController($scope, $state, Authentication, har, $timeout) {
-    var vm = this;
+    HarsController.$inject = ['$scope', '$state', 'Authentication',
+        'harResolve', '$timeout', '$uibModal'
+    ];
+
+    function HarsController($scope, $state, Authentication, har, $timeout, $uibModal) {
+        var vm = this;
 
 
 
-    vm.authentication = Authentication;
-    vm.har = har;
-    vm.error = null;
-    vm.form = {};
-    vm.remove = remove;
-    vm.swaggerfy = swaggerfy;
-    vm.plantify = plantify;
-    vm.showUml = showUml;
-    vm.save = save;
-    vm.harText = JSON.stringify(har.log, null, 2);
+        vm.authentication = Authentication;
+        vm.har = har;
+        vm.error = null;
+        vm.form = {};
+        vm.remove = remove;
+        vm.swaggerfy = swaggerfy;
+        vm.plantify = plantify;
+        vm.save = save;
+        vm.harText = JSON.stringify(har.log, null, 2);
+        vm.closeModal = closeErrorModal;
 
-    if (vm.har.puml) vm.showUml();
+        if (vm.har.puml) showUml();
 
-    $scope.aceChanged = function (e) {
-      vm.har.log = JSON.parse(vm.harText);
-    };
+        $scope.aceChanged = function(e) {
+            vm.har.log = JSON.parse(vm.harText);
+        };
 
-    $scope.aceLoaded = function (_editor) {
-      _editor.setValue(vm.harText);
-      _editor.focus(); // To focus the ace editor
-      _editor.$blockScrolling = Infinity;
-      _editor.selection.moveTo(0, 0);
-    };
+        $scope.aceLoaded = function(_editor) {
+            _editor.setValue(vm.harText);
+            _editor.focus(); // To focus the ace editor
+            _editor.$blockScrolling = Infinity;
+            _editor.selection.moveTo(0, 0);
+        };
 
-    // Remove existing Har
-    function remove() {
-      if (confirm('Are you sure you want to delete?')) {
-        vm.har.$remove($state.go('hars.list'));
-      }
-    }
-
-    // Convert existing Har to Swagger
-    function swaggerfy() {
-      vm.har.$swaggerfy(
-        $state.go('specs.list')
-      );
-    }
-
-    // Convert existing Har to UML Class Diagram
-    function plantify() {
-      vm.har.$plantify({},
-        function (data) {
-          vm.showUml();
+        function showErrorModal() {
+            vm.errorModalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'errorModal.html',
+                scope: $scope,
+            });
         }
-      );
+
+        function closeErrorModal() {
+            vm.errorModalInstance.dismiss('dismiss');
+        }
+
+        // Remove existing Har
+        function remove() {
+            if (confirm('Are you sure you want to delete?')) {
+                vm.har.$remove($state.go('hars.list'));
+            }
+        }
+
+        // Convert existing Har to Swagger
+        function swaggerfy() {
+
+            vm.har.$swaggerfy({}, successCallback, errorCallback);
+
+            function successCallback(res) {
+                $state.go('specs.list');
+            }
+
+            function errorCallback(res) {
+                $scope.errorHeader = res.data.message.name;
+                $scope.errorText = res.data.message.message;
+                showErrorModal();
+            }
+        }
+
+        // Convert existing Har to UML Class Diagram
+        function plantify() {
+            vm.har.$plantify({}, successCallback, errorCallback);
+
+            function successCallback(res) {
+                showUml();
+            }
+
+            function errorCallback(res) {
+                vm.error = res.data.message;
+            }
+        }
+
+        // load up the umlImageUrl with timestamp to force a refresh of the binding.
+        function showUml() {
+            //TODO get this right!
+            $timeout(function() {
+                $scope.umlImageUrl = "/api/hars/" + vm.har._id + "/puml" + "?" + new Date().getTime();
+            });
+        }
+
+        // Save Har
+        function save(isValid) {
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'vm.form.harForm');
+                return false;
+            }
+
+            // TODO: move create/update logic to service
+            if (vm.har._id) {
+                vm.har.$update(successCallback, errorCallback);
+            } else {
+                vm.har.$save(successCallback, errorCallback);
+            }
+
+            function successCallback(res) {
+                $state.go('hars.view', {
+                    harId: res._id
+                });
+            }
+
+            function errorCallback(res) {
+                vm.error = res.data.message;
+            }
+        }
+
     }
-
-    // Convert existing Har to UML Class Diagram
-    function showUml() {
-      //TODO get this right!
-      $timeout(function () {
-        //$scope.umlImageUrl = $scope.umlImageUrl + '?' + new Date().getTime();
-        $scope.umlImageUrl = "/api/hars/" + vm.har._id + "/puml" + "?" + new Date().getTime();
-      });
-    }
-
-    // Save Har
-    function save(isValid) {
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'vm.form.harForm');
-        return false;
-      }
-
-      // TODO: move create/update logic to service
-      if (vm.har._id) {
-        vm.har.$update(successCallback, errorCallback);
-      } else {
-        vm.har.$save(successCallback, errorCallback);
-      }
-
-      function successCallback(res) {
-        $state.go('hars.view', {
-          harId: res._id
-        });
-      }
-
-      function errorCallback(res) {
-        vm.error = res.data.message;
-      }
-    }
-
-  }
 })();
--- modules/hars/server/controllers/hars.server.controller.js
+++ modules/hars/server/controllers/hars.server.controller.js
@@ -4,252 +4,255 @@
  * Module dependencies.
  */
 var path = require('path'),
-  mongoose = require('mongoose'),
-  http = require('http'),
-  Har = mongoose.model('Har'),
-  Spec = mongoose.model('Spec'),
-  h2s = require('har-to-swagger'),
-  plantuml = require('node-plantuml'),
-  pumlEncoder = require('plantuml-encoder'),
-  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-  _ = require('lodash');
+    mongoose = require('mongoose'),
+    http = require('http'),
+    Har = mongoose.model('Har'),
+    Spec = mongoose.model('Spec'),
+    h2s = require('har-to-swagger'),
+    plantuml = require('node-plantuml'),
+    pumlEncoder = require('plantuml-encoder'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+    _ = require('lodash');
 
 /**
  * Create a Har
  */
-exports.create = function (req, res) {
-  var har = new Har(req.body);
-  har.user = req.user;
-  har.save(function (err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(har);
-    }
-  });
-
-};
-
-exports.createSwagger = function (req, res) {
-  var user = req.user,
-    har = req.har,
-    logObj = {
-      log: har.log
-    },
-    harId = har._id;
-  // console.log(JSON.stringify(logObj, null, 2));
-
-  h2s.generateAsync(JSON.stringify(logObj), function (err, result) {
-    if (err) {
-      console.log('error: ' + err);
-      return res.status(400).send({
-        message: err
-      });
-    } else {
-      var spec = new Spec();
-      spec.user = req.user;
-      spec.title = har.name;
-      spec.swagger = result.swagger;
-      spec.save(function (err) {
+exports.create = function(req, res) {
+    var har = new Har(req.body);
+    har.user = req.user;
+    har.save(function(err) {
         if (err) {
-          return res.status(400).send({
-            message: err
-          });
-        } else {
-          har.specs.push(spec._id);
-          har.save(function (err) {
-            if (err) {
-              return res.status(400).send({
+            return res.status(400).send({
                 message: errorHandler.getErrorMessage(err)
-              });
-            } else {
-              res.jsonp(har);
-            }
-          });
+            });
+        } else {
+            res.jsonp(har);
         }
-      });
-    }
-  });
-
-};
-
-exports.readUML = function (req, res) {
-  var har = req.har;
-  // res.set('Content-Type', 'image/svg+xml');
-  res.set('Content-Type', 'image/png');
-  var decode = plantuml.decode(har.puml);
-  // var gen = plantuml.generate({format: 'svg'});
-  var gen = plantuml.generate({ format: 'png' });
-  decode.out.pipe(gen.in);
-  gen.out.pipe(res);
-};
-
-exports.createUML = function (req, res) {
-  var har = req.har;
-  var log = har.log;
-  var pumlText = '';
-  pumlfy(log, function (data) {
-    pumlText = data;
-    var encoded = pumlEncoder.encode(pumlText);
-    har.puml = encoded;
-    har.save(function (err) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.jsonp(har);
-      }
     });
-  });
+
+};
+
+exports.createSwagger = function(req, res) {
+    var user = req.user,
+        har = req.har,
+        logObj = {
+            log: har.log
+        },
+        harId = har._id;
+    // console.log(JSON.stringify(logObj, null, 2));
+
+    h2s.generateAsync(JSON.stringify(logObj), function(err, result) {
+        if (err) {
+            console.log('error: ' + err);
+            return res.status(400).send({
+                message: err
+            });
+        } else {
+            var spec = new Spec();
+            spec.user = req.user;
+            spec.title = har.name;
+            spec.swagger = result.swagger;
+            spec.save(function(err) {
+                if (err) {
+                  console.log('Error saving spec to MongoDB: ' + JSON.stringify(spec, null, 2));
+                    return res.status(400).send({
+                        message: err
+                    });
+                } else {
+                    har.specs.push(spec._id);
+                    har.save(function(err) {
+                        if (err) {
+                            return res.status(400).send({
+                                message: errorHandler.getErrorMessage(err)
+                            });
+                        } else {
+                            res.jsonp(har);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+};
+
+exports.readUML = function(req, res) {
+    var har = req.har;
+    // res.set('Content-Type', 'image/svg+xml');
+    res.set('Content-Type', 'image/png');
+    var decode = plantuml.decode(har.puml);
+    // var gen = plantuml.generate({format: 'svg'});
+    var gen = plantuml.generate({ format: 'png' });
+    decode.out.pipe(gen.in);
+    gen.out.pipe(res);
+};
+
+exports.createUML = function(req, res) {
+    var har = req.har;
+    var log = har.log;
+    var pumlText = '';
+    pumlfy(log, function(data) {
+        pumlText = data;
+        var encoded = pumlEncoder.encode(pumlText);
+        har.puml = encoded;
+        har.save(function(err) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.jsonp(har);
+            }
+        });
+    });
 };
 
 function pumlfy(log, callback) {
-  var pumlText = '';
-  for (var entry in log.entries) {
-    if (log.entries.hasOwnProperty(entry)) {
-      var serviceName = log.entries[entry]['x-harpi-name'];
-      var method = log.entries[entry].request['method'].toLowerCase();
-      switch (method) {
-        case 'get':
-          pumlText += '"User Agent" -> "'
-            + serviceName
-            + '": '
-            + method
-            + '('
-            + log.entries[entry].request['x-harpi-description']
-            + ') \n'
-            + serviceName
-            + ' -> "User Agent": '
-            + log.entries[entry].response.status
-            + '('
-            + log.entries[entry].response.content['x-harpi-description']
-            + ') \n';
-          break;
-        case 'put':
-          pumlText += '"User Agent" -> "'
-            + serviceName
-            + '": '
-            + method
-            + '('
-            + log.entries[entry].request.postData['x-harpi-description']
-            + ') \n'
-            + serviceName
-            + ' -> "User Agent": '
-            + log.entries[entry].response.status
-            + '('
-            + log.entries[entry].response.content['x-harpi-description']
-            + ') \n';
-          break;
-        default:
-          pumlText += '"User Agent" -> "'
-            + serviceName
-            + '": '
-            + method
-            + '() \n'
-            + serviceName
-            + ' -> "User Agent": '
-            + log.entries[entry].response.status
-            + '( '
-            + log.entries[entry].response.content['x-harpi-description']
-            + ' ) \n';
-          break;
-      }
+    var pumlText = '';
+    for (var entry in log.entries) {
+        if (log.entries.hasOwnProperty(entry)) {
+            var serviceName = log.entries[entry]['x-name'];
+            var method = log.entries[entry].request.method.toLowerCase();
+            switch (method) {
+                case 'get':
+                    pumlText +=
+                        '"User Agent" -> "' +
+                        serviceName +
+                        '": ' +
+                        method +
+                        '(' +
+                        log.entries[entry].request['x-description'] +
+                        ') \n' +
+                        serviceName +
+                        ' -> "User Agent": ' +
+                        log.entries[entry].response.status +
+                        '(' +
+                        log.entries[entry].response.content['x-description'] +
+                        ') \n';
+                    break;
+                case 'put':
+                    pumlText +=
+                        '"User Agent" -> "' +
+                        serviceName +
+                        '": ' +
+                        method +
+                        '(' +
+                        log.entries[entry].request.postData['x-description'] +
+                        ') \n' +
+                        serviceName +
+                        ' -> "User Agent": ' +
+                        log.entries[entry].response.status +
+                        '(' +
+                        log.entries[entry].response.content['x-description'] +
+                        ') \n';
+                    break;
+                default:
+                    pumlText += '"User Agent" -> "' +
+                        serviceName +
+                        '": ' +
+                        method +
+                        '() \n' +
+                        serviceName +
+                        ' -> "User Agent": ' +
+                        log.entries[entry].response.status +
+                        '( ' +
+                        log.entries[entry].response.content['x-description'] +
+                        ' ) \n';
+                    break;
+            }
+        }
     }
-  }
-  // Make sure the callback is a function​
-  if (typeof callback === 'function') {
-    // Call it, since we have confirmed it is callable​
-    callback(pumlText);
-  }
-};
+    // Make sure the callback is a function​
+    if (typeof callback === 'function') {
+        // Call it, since we have confirmed it is callable​
+        callback(pumlText);
+    }
+}
 
 /**
  * Show the current Har
  */
-exports.read = function (req, res) {
-  // convert mongoose document to JSON
-  var har = req.har ? req.har.toJSON() : {};
-  // Add a custom field to Har, for determining if the current User is the "owner".
-  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Har model.
-  har.isCurrentUserOwner = req.user && har.user && har.user._id.toString() === req.user._id.toString() ? true : false;
+exports.read = function(req, res) {
+    // convert mongoose document to JSON
+    var har = req.har ? req.har.toJSON() : {};
+    // Add a custom field to Har, for determining if the current User is the "owner".
+    // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Har model.
+    har.isCurrentUserOwner = req.user && har.user && har.user._id.toString() === req.user._id.toString() ? true : false;
 
-  res.jsonp(har);
+    res.jsonp(har);
 };
 
 /**
  * Update a Har
  */
-exports.update = function (req, res) {
-  var har = req.har;
+exports.update = function(req, res) {
+    var har = req.har;
 
-  har = _.extend(har, req.body);
+    har = _.extend(har, req.body);
 
-  har.save(function (err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(har);
-    }
-  });
+    har.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(har);
+        }
+    });
 };
 
 /**
  * Delete an Har
  */
-exports.delete = function (req, res) {
-  var har = req.har;
+exports.delete = function(req, res) {
+    var har = req.har;
 
-  har.remove(function (err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(har);
-    }
-  });
+    har.remove(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(har);
+        }
+    });
 };
 
 /**
  * List of Hars
  */
-exports.list = function (req, res) {
-  Har.find().sort('-created').populate('user', 'displayName').exec(function (err, hars) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(hars);
-    }
-  });
+exports.list = function(req, res) {
+    Har.find().sort('-created').populate('user', 'displayName').exec(function(err, hars) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(hars);
+        }
+    });
 };
 
 /**
  * Har middleware
  */
-exports.harByID = function (req, res, next, id) {
+exports.harByID = function(req, res, next, id) {
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({
-      message: 'Har is invalid'
-    });
-  }
-
-  Har.findById(id).populate('user', 'displayName').exec(function (err, har) {
-    if (err) {
-      return next(err);
-    } else if (!har) {
-      return res.status(404).send({
-        message: 'No Har with that identifier has been found'
-      });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({
+            message: 'Har is invalid'
+        });
     }
-    req.har = har;
-    next();
-  });
+
+    Har.findById(id).populate('user', 'displayName').exec(function(err, har) {
+        if (err) {
+            return next(err);
+        } else if (!har) {
+            return res.status(404).send({
+                message: 'No Har with that identifier has been found'
+            });
+        }
+        req.har = har;
+        next();
+    });
 };
