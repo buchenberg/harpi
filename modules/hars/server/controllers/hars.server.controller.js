4cc7705ac278a0e18ae75e36e89fa27b8f52e807
Greg Buchenberger <greggbuchenberger@admin.local>
2016-09-22 15:02:03 -0600

    formatting

--- modules/hars/server/controllers/hars.server.controller.js
+++ modules/hars/server/controllers/hars.server.controller.js
@@ -4,255 +4,273 @@
  * Module dependencies.
  */
 var path = require('path'),
-    mongoose = require('mongoose'),
-    http = require('http'),
-    Har = mongoose.model('Har'),
-    Spec = mongoose.model('Spec'),
-    h2s = require('har-to-swagger'),
-    plantuml = require('node-plantuml'),
-    pumlEncoder = require('plantuml-encoder'),
-    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-    _ = require('lodash');
+  mongoose = require('mongoose'),
+  http = require('http'),
+  Har = mongoose.model('Har'),
+  Spec = mongoose.model('Spec'),
+  h2s = require('har-to-swagger'),
+  plantuml = require('node-plantuml'),
+  pumlEncoder = require('plantuml-encoder'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  _ = require('lodash');
 
 /**
  * Create a Har
  */
-exports.create = function(req, res) {
-    var har = new Har(req.body);
-    har.user = req.user;
-    har.save(function(err) {
-        if (err) {
-            return res.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-            });
-        } else {
-            res.jsonp(har);
-        }
-    });
+exports.create = function (req, res) {
+  var har = new Har(req.body);
+  har.user = req.user;
+  har.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(har);
+    }
+  });
 
 };
 
-exports.createSwagger = function(req, res) {
-    var user = req.user,
-        har = req.har,
-        logObj = {
-            log: har.log
-        },
-        harId = har._id;
+exports.createSwagger = function (req, res) {
+  var user = req.user,
+    har = req.har,
+    logObj = {
+      log: har.log
+    },
+    harId = har._id;
     // console.log(JSON.stringify(logObj, null, 2));
 
-    h2s.generateAsync(JSON.stringify(logObj), function(err, result) {
+  h2s.generateAsync(JSON.stringify(logObj), function (err, result) {
+    if (err) {
+      console.log('error: ' + err);
+      return res.status(400).send({
+        message: err
+      });
+    } else {
+      var spec = new Spec();
+      spec.user = req.user;
+      spec.title = har.name;
+      spec.swagger = result.swagger;
+      spec.save(function (err) {
         if (err) {
-            console.log('error: ' + err);
-            return res.status(400).send({
-                message: err
-            });
+          console.log('Error saving spec to MongoDB: ' + JSON.stringify(spec, null, 2));
+          return res.status(400).send({
+            message: err
+          });
         } else {
-            var spec = new Spec();
-            spec.user = req.user;
-            spec.title = har.name;
-            spec.swagger = result.swagger;
-            spec.save(function(err) {
-                if (err) {
-                  console.log('Error saving spec to MongoDB: ' + JSON.stringify(spec, null, 2));
-                    return res.status(400).send({
-                        message: err
-                    });
-                } else {
-                    har.specs.push(spec._id);
-                    har.save(function(err) {
-                        if (err) {
-                            return res.status(400).send({
-                                message: errorHandler.getErrorMessage(err)
-                            });
-                        } else {
-                            res.jsonp(har);
-                        }
-                    });
-                }
-            });
-        }
-    });
-
-};
-
-exports.readUML = function(req, res) {
-    var har = req.har;
-    // res.set('Content-Type', 'image/svg+xml');
-    res.set('Content-Type', 'image/png');
-    var decode = plantuml.decode(har.puml);
-    // var gen = plantuml.generate({format: 'svg'});
-    var gen = plantuml.generate({ format: 'png' });
-    decode.out.pipe(gen.in);
-    gen.out.pipe(res);
-};
-
-exports.createUML = function(req, res) {
-    var har = req.har;
-    var log = har.log;
-    var pumlText = '';
-    pumlfy(log, function(data) {
-        pumlText = data;
-        var encoded = pumlEncoder.encode(pumlText);
-        har.puml = encoded;
-        har.save(function(err) {
+          har.specs.push(spec._id);
+          har.save(function (err) {
             if (err) {
-                return res.status(400).send({
-                    message: errorHandler.getErrorMessage(err)
-                });
+              return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+              });
             } else {
-                res.jsonp(har);
+              res.jsonp(har);
             }
+          });
+        }
+      });
+    }
+  });
+
+};
+
+exports.readUML = function (req, res) {
+  var har = req.har;
+    // res.set('Content-Type', 'image/svg+xml');
+  res.set('Content-Type', 'image/png');
+  var decode = plantuml.decode(har.puml);
+    // var gen = plantuml.generate({format: 'svg'});
+  var gen = plantuml.generate({ format: 'png' });
+  decode.out.pipe(gen.in);
+  gen.out.pipe(res);
+};
+
+exports.createUML = function (req, res) {
+  var har = req.har;
+  var log = har.log;
+  var pumlText = '';
+  pumlfy(log, function (data) {
+    pumlText = data;
+    var encoded = pumlEncoder.encode(pumlText);
+    har.puml = encoded;
+    har.save(function (err) {
+      if (err) {
+        return res.status(400).send({
+          message: errorHandler.getErrorMessage(err)
         });
+      } else {
+        res.jsonp(har);
+      }
     });
+  });
 };
 
 function pumlfy(log, callback) {
-    var pumlText = '';
-    for (var entry in log.entries) {
-        if (log.entries.hasOwnProperty(entry)) {
-            var serviceName = log.entries[entry]['x-name'];
-            var method = log.entries[entry].request.method.toLowerCase();
-            switch (method) {
-                case 'get':
-                    pumlText +=
-                        '"User Agent" -> "' +
-                        serviceName +
-                        '": ' +
-                        method +
-                        '(' +
-                        log.entries[entry].request['x-description'] +
-                        ') \n' +
-                        serviceName +
-                        ' -> "User Agent": ' +
-                        log.entries[entry].response.status +
-                        '(' +
-                        log.entries[entry].response.content['x-description'] +
-                        ') \n';
-                    break;
-                case 'put':
-                    pumlText +=
-                        '"User Agent" -> "' +
-                        serviceName +
-                        '": ' +
-                        method +
-                        '(' +
-                        log.entries[entry].request.postData['x-description'] +
-                        ') \n' +
-                        serviceName +
-                        ' -> "User Agent": ' +
-                        log.entries[entry].response.status +
-                        '(' +
-                        log.entries[entry].response.content['x-description'] +
-                        ') \n';
-                    break;
-                default:
-                    pumlText += '"User Agent" -> "' +
-                        serviceName +
-                        '": ' +
-                        method +
-                        '() \n' +
-                        serviceName +
-                        ' -> "User Agent": ' +
-                        log.entries[entry].response.status +
-                        '( ' +
-                        log.entries[entry].response.content['x-description'] +
-                        ' ) \n';
-                    break;
-            }
-        }
+  var pumlText = '';
+  for (var entry in log.entries) {
+    if (log.entries.hasOwnProperty(entry)) {
+      var serviceName = log.entries[entry]['x-name'];
+      var method = log.entries[entry].request.method.toLowerCase();
+      var queryParams = '';
+      for (var item in log.entries[entry].request.queryString) {
+        queryParams += log.entries[entry].request.queryString[item].name + ', '
+      }
+      var pathParams = '';
+      for (var item in log.entries[entry].request['x-path-params']) {
+        pathParams += log.entries[entry].request['x-path-params'][item].name + ', '
+      }
+      var allParams = queryParams + pathParams;
+
+      pumlText +=
+              '"User Agent" -> ' +
+              '"' + serviceName + '"' +
+              ': ' + method + '-' + log.entries[entry].request['x-description'] +
+              '(' +
+               allParams.replace(/,\s*$/, '') +
+              ') \n' +
+              'note right: ' +
+              log.entries[entry].request.url +
+               '\n' +
+              '"' + serviceName + '"' +
+              ' -> "User Agent": ' +
+              log.entries[entry].response.status +
+              '(' +
+              log.entries[entry].response['x-description'] +
+              ') \n';
+
+    //   switch (method) {
+    //     case 'get':
+    //       pumlText +=
+    //           '"User Agent" -> ' +
+    //           '"' + serviceName + '"' +
+    //           ': ' + method + '-' + log.entries[entry].request['x-description'] +
+    //           '(' +
+    //            allParams.replace(/,\s*$/, '') +
+    //           ') \n' +
+    //           'note right: ' +
+    //           log.entries[entry].request.url +
+    //            '\n' +
+    //           '"' + serviceName + '"' +
+    //           ' -> "User Agent": ' +
+    //           log.entries[entry].response.status +
+    //           '(' +
+    //           log.entries[entry].response['x-description'] +
+    //           ') \n';
+    //       break;
+    //     default:
+    //       pumlText +=
+    //           '"User Agent" -> ' +
+    //           '"' + serviceName + '"' +
+    //           ': ' + method + '-' + log.entries[entry].request['x-description'] +
+    //           '(' +
+    //           //log.entries[entry].request['x-description'] + //TODO params here
+    //           ') \n' +
+    //           'note right: ' +
+    //           log.entries[entry].request.url +
+    //            '\n' +
+    //           '"' + serviceName + '"' +
+    //           ' -> "User Agent": ' +
+    //           log.entries[entry].response.status +
+    //           '(' +
+    //           log.entries[entry].response['x-description'] +
+    //           ') \n';
+    //       break;
+    //   }
     }
+  }
     // Make sure the callback is a function​
-    if (typeof callback === 'function') {
+  if (typeof callback === 'function') {
         // Call it, since we have confirmed it is callable​
-        callback(pumlText);
-    }
+    callback(pumlText);
+  }
 }
 
 /**
  * Show the current Har
  */
-exports.read = function(req, res) {
+exports.read = function (req, res) {
     // convert mongoose document to JSON
-    var har = req.har ? req.har.toJSON() : {};
+  var har = req.har ? req.har.toJSON() : {};
     // Add a custom field to Har, for determining if the current User is the "owner".
     // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Har model.
-    har.isCurrentUserOwner = req.user && har.user && har.user._id.toString() === req.user._id.toString() ? true : false;
+  har.isCurrentUserOwner = req.user && har.user && har.user._id.toString() === req.user._id.toString() ? true : false;
 
-    res.jsonp(har);
+  res.jsonp(har);
 };
 
 /**
  * Update a Har
  */
-exports.update = function(req, res) {
-    var har = req.har;
+exports.update = function (req, res) {
+  var har = req.har;
 
-    har = _.extend(har, req.body);
+  har = _.extend(har, req.body);
 
-    har.save(function(err) {
-        if (err) {
-            return res.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-            });
-        } else {
-            res.jsonp(har);
-        }
-    });
+  har.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(har);
+    }
+  });
 };
 
 /**
  * Delete an Har
  */
-exports.delete = function(req, res) {
-    var har = req.har;
+exports.delete = function (req, res) {
+  var har = req.har;
 
-    har.remove(function(err) {
-        if (err) {
-            return res.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-            });
-        } else {
-            res.jsonp(har);
-        }
-    });
+  har.remove(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(har);
+    }
+  });
 };
 
 /**
  * List of Hars
  */
-exports.list = function(req, res) {
-    Har.find().sort('-created').populate('user', 'displayName').exec(function(err, hars) {
-        if (err) {
-            return res.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-            });
-        } else {
-            res.jsonp(hars);
-        }
-    });
+exports.list = function (req, res) {
+  Har.find().sort('-created').populate('user', 'displayName').exec(function (err, hars) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(hars);
+    }
+  });
 };
 
 /**
  * Har middleware
  */
-exports.harByID = function(req, res, next, id) {
+exports.harByID = function (req, res, next, id) {
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).send({
-            message: 'Har is invalid'
-        });
-    }
-
-    Har.findById(id).populate('user', 'displayName').exec(function(err, har) {
-        if (err) {
-            return next(err);
-        } else if (!har) {
-            return res.status(404).send({
-                message: 'No Har with that identifier has been found'
-            });
-        }
-        req.har = har;
-        next();
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Har is invalid'
     });
+  }
+
+  Har.findById(id).populate('user', 'displayName').exec(function (err, har) {
+    if (err) {
+      return next(err);
+    } else if (!har) {
+      return res.status(404).send({
+        message: 'No Har with that identifier has been found'
+      });
+    }
+    req.har = har;
+    next();
+  });
 };
--- modules/specs/client/controllers/specs.client.controller.js
+++ modules/specs/client/controllers/specs.client.controller.js
@@ -1,149 +1,149 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    // Specs controller
-    angular
-        .module('specs')
-        .controller('SpecsController', SpecsController);
+  // Specs controller
+  angular
+    .module('specs')
+    .controller('SpecsController', SpecsController);
 
-    SpecsController.$inject = ['$scope', '$state', 'Authentication', 'specResolve', '$uibModal'];
+  SpecsController.$inject = ['$scope', '$state', 'Authentication', 'specResolve', '$uibModal'];
 
-    function SpecsController($scope, $state, Authentication, spec, $uibModal) {
-        var vm = this;
+  function SpecsController($scope, $state, Authentication, spec, $uibModal) {
+    var vm = this;
 
-        vm.authentication = Authentication;
-        vm.spec = spec;
-        vm.error = null;
-        vm.form = {};
-        vm.remove = remove;
-        vm.save = save;
-        vm.validateSwagger = validateSwagger;
-        vm.testSwagger = testSwagger;
-        vm.dereferenceSwagger = dereferenceSwagger;
-        vm.swaggerText = JSON.stringify(spec.swagger, null, 2);
-        vm.resultText = '';
-        vm.modalInstance = null;
-        vm.closeModal = closeModal;
-        //vm.getDreddReportPartial = getDreddReportPartial;
-
-
-
-        $scope.swaggerUrl = window.location.origin + '/api/specs/' + vm.spec._id + '/swagger';
-
-        $scope.aceLoaded = function(_editor) {
-            _editor.setValue(vm.swaggerText);
-            _editor.focus(); // To focus the ace editor
-            _editor.$blockScrolling = Infinity;
-            _editor.selection.moveTo(0, 0);
-            vm.editor = _editor;
-        };
-
-        $scope.aceChanged = function(e) {
-            vm.spec.swagger = JSON.parse(vm.swaggerText);
-        };
-
-        // function getDreddReportPartial() {
-        //     return './modules/specs/client/views/' + vm.spec._id + '-result.html';
-        // }
-
-        function openModal() {
-            vm.modalInstance = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'modal.html',
-                scope: $scope,
-            });
-        }
-
-        function closeModal() {
-            vm.modalInstance.dismiss('dismiss');
-        }
-
-        // Remove existing Spec
-        function remove() {
-            if (confirm('Are you sure you want to delete?')) {
-                vm.spec.$remove($state.go('specs.list'));
-            }
-        }
-
-        // Validate Swagger 2.0
-        function validateSwagger() {
-            SwaggerParser.validate(vm.spec.swagger)
-                .then(successCallback)
-                .catch(errorCallback);
-
-            function successCallback(res) {
-                $scope.modalText = 'So Swagger. Much valid.';
-                $scope.modalHeader = 'Pwnage!';
-                openModal();
-            }
-
-            function errorCallback(res) {
-                $scope.modalText = res.message;
-                $scope.modalHeader = 'O Noes!';
-                openModal();
-            }
-        }
-
-        // Dredd test Swagger 2.0
-        function testSwagger() {
-            //vm.spec.$test({}, successCallback, errorCallback);
-            $scope.modalText = "foooooooo";
-            $scope.modalHeader = 'Pwnage!';
-            openModal();
-
-            function successCallback(res) {
-                $scope.modalText = "foooooooo";
-                $scope.modalHeader = 'Pwnage!';
-                openModal();
-                //$scope.activeTab = 1;
-            }
-
-            function errorCallback(res) {
-                $scope.modalText = res.data.error;
-                $scope.modalHeader = 'O Noes!';
-                openModal();
-            }
-        }
-
-        // Dereference
-        function dereferenceSwagger(_editor) {
-            SwaggerParser.dereference(vm.spec.swagger)
-                .then(function(api) {
-                    var dereferencedSwagger = JSON.stringify(api, null, 2);
-                    vm.editor.setValue(dereferencedSwagger, 1);
-                });
-        }
-
-        // Save Spec
-        function save(isValid) {
-            if (!isValid) {
-                $scope.$broadcast('show-errors-check-validity', 'vm.form.specForm');
-                return false;
-            }
-            SwaggerParser.validate(vm.spec.swagger)
-                .then(function(api) {
-                    if (vm.spec._id) {
-                        vm.spec.$update(successCallback, errorCallback);
-                    } else {
-                        vm.spec.$save(successCallback, errorCallback);
-                    }
-                })
-                .catch(function(err) {
-                    vm.error = err.message;
-                });
+    vm.authentication = Authentication;
+    vm.spec = spec;
+    vm.error = null;
+    vm.form = {};
+    vm.remove = remove;
+    vm.save = save;
+    vm.validateSwagger = validateSwagger;
+    vm.testSwagger = testSwagger;
+    vm.dereferenceSwagger = dereferenceSwagger;
+    vm.swaggerText = JSON.stringify(spec.swagger, null, 2);
+    vm.resultText = '';
+    vm.modalInstance = null;
+    vm.closeModal = closeModal;
+    //vm.getDreddReportPartial = getDreddReportPartial;
 
 
-            function successCallback(res) {
-                $state.go('specs.view', {
-                    specId: res._id
-                });
-            }
 
-            function errorCallback(res) {
-                vm.error = res.data.message;
-            }
-        }
+    $scope.swaggerUrl = window.location.origin + '/api/specs/' + vm.spec._id + '/swagger';
+
+    $scope.aceLoaded = function (_editor) {
+      _editor.setValue(vm.swaggerText);
+      _editor.focus(); // To focus the ace editor
+      _editor.$blockScrolling = Infinity;
+      _editor.selection.moveTo(0, 0);
+      vm.editor = _editor;
+    };
+
+    $scope.aceChanged = function (e) {
+      vm.spec.swagger = JSON.parse(vm.swaggerText);
+    };
+
+    // function getDreddReportPartial() {
+    //     return './modules/specs/client/views/' + vm.spec._id + '-result.html';
+    // }
+
+    function openModal() {
+      vm.modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: 'modal.html',
+        scope: $scope,
+      });
     }
+
+    function closeModal() {
+      vm.modalInstance.dismiss('dismiss');
+    }
+
+    // Remove existing Spec
+    function remove() {
+      if (confirm('Are you sure you want to delete?')) {
+        vm.spec.$remove($state.go('specs.list'));
+      }
+    }
+
+    // Validate Swagger 2.0
+    function validateSwagger() {
+      SwaggerParser.validate(vm.spec.swagger)
+        .then(successCallback)
+        .catch(errorCallback);
+
+      function successCallback(res) {
+        $scope.modalText = 'So Swagger. Much valid.';
+        $scope.modalHeader = 'Pwnage!';
+        openModal();
+      }
+
+      function errorCallback(res) {
+        $scope.modalText = res.message;
+        $scope.modalHeader = 'O Noes!';
+        openModal();
+      }
+    }
+
+    // Dredd test Swagger 2.0
+    function testSwagger() {
+      //vm.spec.$test({}, successCallback, errorCallback);
+      $scope.modalText = "foooooooo";
+      $scope.modalHeader = 'Pwnage!';
+      openModal();
+
+      function successCallback(res) {
+        $scope.modalText = "foooooooo";
+        $scope.modalHeader = 'Pwnage!';
+        openModal();
+        //$scope.activeTab = 1;
+      }
+
+      function errorCallback(res) {
+        $scope.modalText = res.data.error;
+        $scope.modalHeader = 'O Noes!';
+        openModal();
+      }
+    }
+
+    // Dereference
+    function dereferenceSwagger(_editor) {
+      SwaggerParser.dereference(vm.spec.swagger)
+        .then(function (api) {
+          var dereferencedSwagger = JSON.stringify(api, null, 2);
+          vm.editor.setValue(dereferencedSwagger, 1);
+        });
+    }
+
+    // Save Spec
+    function save(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.form.specForm');
+        return false;
+      }
+      SwaggerParser.validate(vm.spec.swagger)
+        .then(function (api) {
+          if (vm.spec._id) {
+            vm.spec.$update(successCallback, errorCallback);
+          } else {
+            vm.spec.$save(successCallback, errorCallback);
+          }
+        })
+        .catch(function (err) {
+          vm.error = err.message;
+        });
+
+
+      function successCallback(res) {
+        $state.go('specs.view', {
+          specId: res._id
+        });
+      }
+
+      function errorCallback(res) {
+        vm.error = res.data.message;
+      }
+    }
+  }
 })();
